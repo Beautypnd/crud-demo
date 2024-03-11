6d0a6ad7ffcb06fe7bc33f750caff065ee9427d5
Beauty Kumari <99247882+Beautypnd@users.noreply.github.com>
2024-03-11 17:02:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import Challenges from "./Components/Challenges";
-import Dev  from "./Components/Dev";
-import FindCoder from "./Components/FindCoder";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Hire from "./Components/Hire";
-import Explore from "./Components/Explore";
 import Home from "./CURD/Home";
-// import Logo from "./Components/Logo";
 import Users from "./CURD/Users";
 import CreateUsers from "./CURD/CreateUsers";
 import EditPage from "./CURD/EditPage";
 
 const App = () => {
   return (
-    <div>
-      {/* this is for FindCoder app it is present in components foler */}
-      {/* <BrowserRouter>
-        <FindCoder />
-        <Routes>
-            <Route element={<Explore/>} path="/"/>
-            <Route element={<Hire/>} path="/a"/>
-            <Route element={<Dev/>} path="/dev"/>
-            <Route element={<Challenges/>} path="/c"/>
-        </Routes>
-      </BrowserRouter> */}
+    
 
       <BrowserRouter>
       <Home/>
